86011c2917c4adcd806e861c8287af8fd4ecb9b1
ken7875 <ken19970723@gmail.com>
2026-01-17 17:56:58 +0800

    feat: vue-tsc type check

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  typescript: {
+    strict: true,
+    typeCheck: true,
+  },
 
   modules: [
     '@nuxtjs/i18n',
